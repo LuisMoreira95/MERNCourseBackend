e77939839888570a801a93748821a90bcf7d1023
Luís Moreira <llmoreira@parceiros.nos.pt>
2023-05-19 18:44:49 +0100

    first commit

---
+++ util/location.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+
+const API_KEY = process.env.GOOGLE_API_KEY; // API_KEY = "AIzaSyDI3ug5xel7ezjoFlDykXypyV-TuGdjzrc";
+
+async function getCoordsForAddress(address) {
+  // const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodeURIComponent(
+  //   address
+  // )}&inputtype=textquery&fields=geometry&key=${API_KEY}`;
+
+  // const response = await axios.get(url);
+
+  // const data = response.data;
+  // console.log("DTA", data);
+  // if (!data || data.status === "ZERO_RESULTS") {
+  //   const error = new HttpError(
+  //     "Could not find location for the specified address.",
+  //     422
+  //   );
+  //   throw error;
+  // }
+  // const coordinates = data.candidates[0].geometry.location;
+
+  const coordinates = {
+    lat: 40.7484474,
+    lng: -73.9871516,
+  };
+
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
